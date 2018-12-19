321270bcc49aa70b011881bcfe3eba5b14c4b1bc
Scotty <35357053+ScottCaruso123@users.noreply.github.com>
2018-12-18 19:14:51 -0700

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,4 @@
-
-d3.csv("/Users/scottcaruso/Desktop/jsVisDash/dataset/belly_button_metadata.csv", function(data) {
- let rows = data; 
-});
-
-function buildMetadata(samples) {
+function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
@@ -18,25 +13,26 @@ function buildMetadata(samples) {
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata
+    d3.json(`metadata/${sample}`).then(d => {
+      console.log(d);
+    
+    for (var key in d){
+      h6Tag = document.createElement("h6");
+      h6Text = document.createTextNode(`${key}: ${d[key]}`);
+      h6Tag.append(h6Text);
+      panel.appendChild(h6Tag);
 
-    // for (var key in samples){
-    //   h6Tag = document.createElement("h6");
-    //   h6Text = document.createTextNode(`${key}: ${samples[key]}`);
-    //   h6Tag.append(h6Text);
-    //   panel.appendChild(h6Tag);
+    }
+//     var table = d3.select('#sample-metadata')
 
-    // }
-
-    var table = d3.select("#sample-metadata")
-      .data(data)
-      .enter()
-      .append("#sample-metadata")
-      .text(function(d) {return d; 
-      });
-
+//     Object.keys(d).forEach(function (key) {
+//       table.append('h3').text(d[key]);
+//   // do something with key or value
+// });
+ 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+})};
 
 
 function buildCharts(sample) {
@@ -44,13 +40,60 @@ function buildCharts(sample) {
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   d3.json(`samples/${sample}`).then(d => {
     console.log(d);
-  });
+
+  
+
+
+    
     // @TODO: Build a Bubble Chart using the sample data
+
+    var trace1 = {
+      x:d['otu_ids'].slice(0, 10),
+      y:d['sampel_values'],
+      mode:'markers',
+      text:d['otu_labels'],
+      marker: {
+        size: d['sample_values'],
+        color: d['otu_ids'],
+        colorscale: "Earth",
+
+      }
+    };
+
+    var bubbleData = [trace1];
+// Start of the bubble chart 
+    var layout = {
+      title:"Bubble Chart",
+      showlegend: true,
+      xaxis: {title:'x axis'},
+      yaxis: {title: 'y axis'},
+      height: 600,
+      width:1200
+    };
+
+    Plotly.newPlot("bubble", bubbleData, layout);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+// beginning of pie chart
+
+    var pieData = [{
+      values: d['sample_values'].slice(0, 10),
+      labels: d['otu_ids'].slice(0,10),
+      hovertext: d['otu_labels'].slice(0,10),
+      hoverinfo: 'hovertext',
+      type: 'pie'
+    }];
+    var pieLayout = {
+      margin:{t: 0, l: 0 }
+    };
+
+    var pieChart = document.getElementById('pie');
+    Plotly.plot(pieChart, pieData, pieLayout);
+})};
+
+
 
 function init() {
   // Grab a reference to the dropdown select element
